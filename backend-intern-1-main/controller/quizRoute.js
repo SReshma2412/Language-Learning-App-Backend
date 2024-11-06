f17a886a41098b9ea80b0965ce8e6d2e846a8f9c
SReshma2412 <reshmaresh7604@gmail.com>
2024-11-06 19:34:01 +0530

    first commit

---
+++ backend-intern-1-main/controller/quizRoute.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const quizSchema = require("../model/quizSchema");
+const quizRoute = express.Router();
+const mongoose = require("mongoose");
+quizRoute.post("/create-quiz",(req,res)=>{
+  quizSchema.create(req.body,(err,data)=>{
+    if(err)
+     return err;
+    else
+     res.json(data);
+  })
+})
+quizRoute.get("/",(req,res)=>{
+    quizSchema.find((err,data)=>{
+        if(err)
+          return err;
+        else
+          res.json(data);
+    })
+})
+quizRoute.route("/update-quiz/:id")
+.get((req,res)=>{quizSchema.findById(mongoose.Types.ObjectId(req.params.id),(err,data)=>
+{
+console.log("Inside update quiz");
+console.log(req.body);
+if(err)
+{
+console.log(err)
+return err;
+}
+else
+{
+  console.log("successfully sent");
+  console.log(data);
+res.json(data);
+}
+})
+}).put((req,res)=>{
+  console.log("Inside update quiz mongodb");
+  console.log(req.body);
+quizSchema.findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id)
+,
+{$set: req.body},
+(err,data)=>{
+if(err){
+console.log(err);
+return err;
+}
+else{
+  console.log("successfully edited");
+  console.log(data);
+res.json(data);
+}
+})
+})
+quizRoute.delete("/delete-quiz/:id",(req,res)=>{
+quizSchema.findByIdAndRemove(mongoose.Types.ObjectId(req.params.id)
+,
+(err,data)=>{
+if(err)
+return err;
+else
+res.json(data);
+})
+})
+module.exports=quizRoute;
---
+++ backend-intern-1-main/controller/registerRoute.js
@@ -0,0 +1,20 @@
+// //
+
+// registerRoute.post("/register",(req,res)=>{
+//     registerSchema.create(req.body,(err,data)=>{
+//         if(err)
+//           return err;
+//         else
+//          res.json(data);
+//     })
+// })
+// registerRoute.get("/",(req,res)=>{
+//     registerSchema.find((err,data)=>{
+//         if(err)
+//          return err;
+//         else
+//          res.json(data);
+//     })
+// })
+
+//module.exports=registerRoute;
